950019776ca11f762500878ae6ce96a325ad3d64
Fabian Feitsch <info@fafeitsch.de>
2022-11-14 19:16:32 +0100

    feat: start on reading the current task of vehicles

---
+++ backend/vehicle/timestring.go
@@ -0,0 +1,35 @@
+package vehicle
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type timeString string
+
+func convertToTimeStr(s string) (timeString, error) {
+	regex := regexp.MustCompile("(\\d?\\d):(\\d\\d)")
+	match := regex.FindAllStringSubmatch(s, -1)
+	errorMessage := fmt.Errorf("\"%s\" is no valid timestring. must be of format %s", s, regex.String())
+	if match == nil || len(match) != 1 {
+		return "", errorMessage
+	}
+	hours, _ := strconv.Atoi(match[0][0])
+	if hours > 23 {
+		return "", fmt.Errorf("\"%s\" is not a valid time because the hours are greater than 23", s)
+	}
+	minutes, _ := strconv.Atoi(match[0][1])
+	if minutes > 60 {
+		return "", fmt.Errorf("\"%s\" is not a valid time because the minutes are greater than 60", s)
+	}
+	return timeString(s), nil
+}
+
+func (t timeString) parseTime() (int, int) {
+	split := strings.Split(string(t), ":")
+	hours, _ := strconv.Atoi(split[0])
+	minutes, _ := strconv.Atoi(split[1])
+	return minutes, hours
+}
--- backend/vehicle/vehicle.go
+++ backend/vehicle/vehicle.go
@@ -14,12 +14,21 @@ type latLng struct {
 	lng float64
 }
 
+type waypoint struct {
+	latLng
+	dist  float64
+	dur   float64
+	stop  bool
+	start string
+}
+
 type Vehicle struct {
-	Path      string
-	Key       string
-	position  latLng
-	taskIndex int
-	task      persistence.Task
+	Path        string
+	Key         string
+	position    latLng
+	taskIndex   int
+	tasks       []persistence.Task
+	currentTask []waypoint
 }
 
 func NewVehicle(path string, key string) (*Vehicle, error) {
@@ -45,7 +54,72 @@ func NewVehicle(path string, key string) (*Vehicle, error) {
 		Key:       key,
 		position:  latLng{lat: position[0], lng: position[1]},
 		taskIndex: 0,
-		task:      persisted.Tasks[0],
+		tasks:     persisted.Tasks,
 	}
 	return &result, nil
+}
+
+func (v *Vehicle) loadNextTask() error {
+	if len(v.tasks) == 0 {
+		return fmt.Errorf("no more tasks available")
+	}
+	if v.tasks[0].Type == "roaming" {
+		path, _, err := polyline.DecodeCoords([]byte(v.tasks[0].Path.Geometry))
+		if err != nil {
+			return fmt.Errorf("cannot decode roaming currentTask")
+		}
+		waypoints := make([]waypoint, 0, len(path))
+		for index, wp := range path {
+			waypoints = append(waypoints, waypoint{
+				latLng: latLng{lat: wp[0], lng: wp[1]},
+				dist:   v.tasks[0].Path.Meta[index].Dist,
+				dur:    v.tasks[0].Path.Meta[index].Dur,
+				stop:   false,
+				start:  "0:00",
+			})
+		}
+		v.currentTask = waypoints
+		return nil
+	}
+	timetablePath := filepath.Join(v.Path, "timetables", *v.tasks[0].TimetableKey+".json")
+	timetableFile, err := os.Open(timetablePath)
+	if err != nil {
+		return fmt.Errorf("could not load timetable file \"%s\": %v", timetablePath, err)
+	}
+	defer func() { _ = timetableFile.Close() }()
+	var timetable persistence.Timetable
+	err = json.NewDecoder(timetableFile).Decode(&timetable)
+	if err != nil {
+		return fmt.Errorf("could not read timetable file \"%s\": %v", timetablePath, err)
+	}
+	linePath := filepath.Join(v.Path, "lines", timetable.Line+".json")
+	lineFile, err := os.Open(linePath)
+	if err != nil {
+		return fmt.Errorf("could not load line file \"%s\": %v", linePath, err)
+	}
+	defer func() { _ = lineFile.Close() }()
+	var line persistence.Line
+	err = json.NewDecoder(lineFile).Decode(&line)
+	if err != nil {
+		return fmt.Errorf("could not read line file \"%s\": %v", linePath, err)
+	}
+	path, _, err := polyline.DecodeCoords([]byte(line.Path.Geometry))
+	if err != nil {
+		return fmt.Errorf("could not decode path of line \"%s\": %v", line.Key, err)
+	}
+	waypoints := make([]waypoint, 0, len(path))
+	stopIndex := 0
+	for index, point := range path {
+		wp := waypoint{
+			latLng: latLng{lat: point[0], lng: point[1]},
+			dist:   line.Path.Meta[index].Dist,
+			dur:    line.Path.Meta[index].Dur,
+			stop:   line.Path.Meta[index].Stop,
+			start:  "0:00",
+		}
+		if wp.stop {
+			timetable.Tours[0].
+		}
+	}
+	return nil
 }
